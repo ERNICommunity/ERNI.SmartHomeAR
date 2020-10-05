93265cbc9688c6878a5f7f857eac584df0954b71
vjppaz <vjppaz@gmail.com>
2020-10-05 23:57:12 +0800

    removed apps and some update

--- Assets/Scenes/SmartHomeButtonScript.cs
+++ Assets/Scenes/SmartHomeButtonScript.cs
@@ -4,21 +4,9 @@ using UnityEngine;
 
 public class SmartHomeButtonScript : MonoBehaviour
 {
-    [SerializeField]
-    private string apiKey;
-
-    [SerializeField]
-    private string eventName;
-
-    // Start is called before the first frame update
-    void Start()
+    public void TriggerEvent(string eventName)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log(eventName + " called.");
+        StartCoroutine(IftttMakerManager.Request(eventName));
     }
 }
--- Assets/Scripts/IftttMakerManager.cs
+++ Assets/Scripts/IftttMakerManager.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class IftttMakerManager : MonoBehaviour
 {
     private static string ApiKey = SecretsManager.IftttMakerApiKey;
-    public IEnumerator Request(string eventName)
+    public static IEnumerator Request(string eventName)
     {
         var url = Url(eventName);
+        Debug.Log(url);
+
         using (var www = new WWW(url))
         {
             yield return www;
