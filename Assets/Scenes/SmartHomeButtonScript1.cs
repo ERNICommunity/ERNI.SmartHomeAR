db20e499dc483ef5f049dce8bcc646270717df90
vjppaz <vjppaz@gmail.com>
2020-10-06 23:41:33 +0800

    finalized livingroom tv and added tvcontroller with no lights and ceiling smarthome button draft

---
+++ Assets/Scenes/SmartHomeButtonScript1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartHomeButtonScript : MonoBehaviour
+{
+    public void TriggerEvent(string eventName)
+    {
+        Debug.Log(eventName + " called.");
+        StartCoroutine(IftttMakerManager.Request(eventName));
+    }
+}
