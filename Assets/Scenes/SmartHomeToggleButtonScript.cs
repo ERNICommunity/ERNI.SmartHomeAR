fcf37d2ff81c1490e84809495fd1aba68f48d8c5
vjppaz <vjppaz@gmail.com>
2020-10-07 02:40:09 +0800

    added togglebutton and ceiling prefab

---
+++ Assets/Scenes/SmartHomeToggleButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartHomeToggleButtonScript : MonoBehaviour
+{
+    [SerializeField]
+    private string onEventName;
+
+    [SerializeField]
+    private string offEventName;
+
+    [SerializeField]
+    private bool isOn;
+
+    public void ToggleEvent()
+    {
+        StartCoroutine(IftttMakerManager.Request(isOn ? offEventName : onEventName));
+        isOn = !isOn;
+    }
+}
--- Assets/Scripts/IftttMakerManager.cs
+++ Assets/Scripts/IftttMakerManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class IftttMakerManager : MonoBehaviour
 {
@@ -9,11 +10,13 @@ public class IftttMakerManager : MonoBehaviour
     public static IEnumerator Request(string eventName)
     {
         var url = Url(eventName);
-        Debug.Log(url);
 
-        using (var www = new WWW(url))
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
         {
-            yield return www;
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError) Debug.Log(webRequest.error);
+            else Debug.Log(webRequest.downloadHandler.text);
         }
     }
 
