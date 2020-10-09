9f93d62b9ead77754a00d40d4d853dbad12a0a67
vjppaz <vjppaz@gmail.com>
2020-10-09 20:13:01 +0800

    added nearmenu

---
+++ Assets/Scripts/Debugger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugger : MonoBehaviour
+{
+    public void Log(string message)
+    {
+        Debug.Log(message);
+    }
+}
---
+++ Assets/Scripts/SmartControllerSpawner.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+using System.Collections;
+using System.Collections.Generic;
+
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class SmartControllerSpawner : MonoBehaviour
+{
+    public const string centerObjectTag = "CenterObject";
+
+    [SerializeField]
+    private GameObject prefab;
+
+    private bool isSpawned;
+
+    private GameObject spawnedObject;
+    private GameObject centerObject;
+
+    private TapToPlace spawnedObjectTapToPlace => spawnedObject.GetComponent<TapToPlace>();
+    private Collider spawnedObjectCollider => spawnedObject.GetComponent<Collider>();
+    private Collider centerObjectCollider => centerObject.GetComponent<Collider>();
+
+    public void Spawn()
+    {
+        if (!isSpawned)
+        {
+            spawnedObject = Instantiate(prefab, Vector3.forward, Quaternion.identity);
+            centerObject = FindPrefabChildWithTag(spawnedObject, centerObjectTag);
+        }
+
+        isSpawned = true;
+        Move();
+    }
+
+    public void Move()
+    {
+        spawnedObjectCollider.enabled = true;
+        centerObjectCollider.enabled = false;
+        spawnedObjectTapToPlace.enabled = true;
+        spawnedObjectTapToPlace.StartPlacement();
+    }
+
+    public GameObject FindPrefabChildWithTag(GameObject gameObject, string tag)
+    {
+        foreach (Transform item in gameObject.transform)
+        {
+            if (item.tag == tag)
+            {
+                return item.gameObject;
+            }
+
+            var childGameObject = FindPrefabChildWithTag(item.gameObject, tag);
+            if (childGameObject != null) {
+                return childGameObject;
+            }
+        }
+        return null;
+    }
+}
